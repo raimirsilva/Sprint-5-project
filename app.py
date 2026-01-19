8456f5c633290ea6de17e1210338aa190d497a9e
Raimir Silva <raimir@proton.me>
2026-01-18 23:34:50 -0300

    edit render's suggestions

--- app.py
+++ app.py
@@ -12,16 +12,16 @@ hist_button = st.button('Create a Histogram')
 if hist_button:
     st.write('Creating a Histogram...')
     fig = px.histogram(car_data, x="odometer")
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, width="stretch", key="hist_button_chart")
 
 build_histogram = st.checkbox('Create a histogram')
 if build_histogram:
     st.write('Building histogram for odometer column')
     fig = px.histogram(car_data, x="odometer")
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, width="stretch", key="hist_checkbox_chart")
 
 build_scatter = st.button('Create a scatter plot')
 if build_scatter:
     st.write('Creating scatter plot...')
     fig = px.scatter(car_data, x="odometer", y="price")
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, width="stretch", key="scatter_chart")
